5521944492e8c619c154af3bcd920b5bed77173b
Mvillegas1995 <50243838+Mvillegas1995@users.noreply.github.com>
2019-10-14 16:19:32 -0300

    lunes 14
    up

--- src/vendviajero/Ant.java
+++ src/vendviajero/Ant.java
@@ -2,9 +2,30 @@
 package vendviajero;
 
 public class Ant {
-    private final int[] memoria;
+    private int[] camino; //guarda camino por dónde pasa
+    private int[] memoria; //recuerda si paso por ese vector
     
     public Ant(int tamMemoria){
+        camino = new int[tamMemoria];
         memoria = new int[tamMemoria];
+        for (int i = 0; i < memoria.length; i++) {
+            memoria[i] = 0;
+        }
+    }
+    public void setCamino(int pos, int visitar){
+        camino[pos] = visitar;
+    }
+    public int getCamino(int pos){
+        int ciudad = camino[pos];      
+        return ciudad;
+    }
+    public void borrarMemoria(){
+        for (int i = 0; i < memoria.length; i++) {
+            memoria[i] = 0;
+        }
+    }
+    public float costoCamino(AntColony aC){
+        float costo = aC.costo(camino);
+        return costo;
     }
 }
--- src/vendviajero/AntColony.java
+++ src/vendviajero/AntColony.java
@@ -8,8 +8,9 @@ public final class AntColony {
     private final float alfa; //disolución feromona
     private final float q0;  //probabilidad de explotación o exploración
     private final float beta; //potenciación de matriz N
+    private final float[][] distancia;
     private final float[][] N; //matriz heurística 1/distancia
-    private final Ant[] antVector;
+    public Ant[] antVector;
     private int[] mejorSolucion;
     private float fitnessMejorSolucion;
     private float[][] feromonas;
@@ -19,15 +20,22 @@ public final class AntColony {
         this.alfa = alfa;
         this.q0 = q0;
         this.beta = beta;
-        N = distanciaHeuristica(coordenadas);
+        distancia = distancia(coordenadas);
+        N = new float[distancia.length][distancia.length];
+        for (int i = 0; i < N.length; i++) {
+            for (int j = 0; j < N.length; j++) {
+                if(i == j)
+                    N[i][j] = 0;
+                else{
+                    N[i][j] = 1/distancia[i][j];
+                }
+            }
+        }
         antVector = new Ant[tamColonia];
         for (int i = 0; i < tamColonia; i++) {
             antVector[i] = new Ant(N.length);
         }
         mejorSolucion = new int[N.length];
-        for (int i = 0; i < mejorSolucion.length; i++) { //inicializo mejor Solución
-            mejorSolucion[i] = i;
-        }
         mejorSolucion = desordenarVector(mejorSolucion); //desordeno mejor solucion
         fitnessMejorSolucion = costo(mejorSolucion);    //obtengo el fitness de esa solucion
         feromonas = new float[N.length][N.length];
@@ -40,17 +48,36 @@ public final class AntColony {
             }
         }
     }
-    private int[] desordenarVector(int[] vector){
+    public int[] desordenarVector(int[] vector){
         int rand,temp;
-        for (int i = 0; i < vector.length; i++) {
+        if(vector.length < N.length){
+            int[] memTemp = new int[N.length];
+            for (int i = 0; i < memTemp.length; i++) {
+                memTemp[i] = 0;
+            }
+            for (int i = 0; i < vector.length; i++) {
+                rand = random.nextInt(N.length);
+                while(memTemp[rand] == 1){
+                    rand = random.nextInt(N.length);
+                }
+                vector[i] = rand;
+                memTemp[rand]++;
+            }
+        }
+        else{
+            for (int i = 0; i < vector.length; i++) {
+                vector[i] = i;
+            }
+            for (int i = 0; i < vector.length; i++) {
             temp = vector[i];
             rand = random.nextInt(vector.length);
             vector[i] = vector[rand];
             vector[rand] = temp;
+            }
         }
         return vector;
     }
-    private float[][] distanciaHeuristica(int[][] coordenadas){
+    private float[][] distancia(int[][] coordenadas){
         int cantPuntos = coordenadas.length;
         //MATRIZ DE COSTOS 
         float[][]distancia = new float[cantPuntos][cantPuntos];
@@ -63,7 +90,7 @@ public final class AntColony {
                 else{
                     diferenciaX = coordenadas[i][0] - coordenadas[j][0];
                     diferenciaY = coordenadas[i][1] - coordenadas[j][1];
-                    distancia[i][j] =  (float) (1/(Math.sqrt(Math.pow(diferenciaX, 2) + Math.pow(diferenciaY, 2))));
+                    distancia[i][j] =  (float) Math.sqrt(Math.pow(diferenciaX, 2) + Math.pow(diferenciaY, 2));
                 }
             }
         }
@@ -71,9 +98,13 @@ public final class AntColony {
     }
     public float costo(int[] solucion){
         float costo = 0;
-        for (int i = 0; i < (N.length-1); i++) {
-            costo += N[solucion[i]][solucion[i+1]];
+        for (int i = 0; i < (distancia.length-1); i++) {
+            costo += distancia[solucion[i]][solucion[i+1]];
         }        
         return costo;
     }
+    public float fitness(){
+        return fitnessMejorSolucion;
+    }
+    
 }
